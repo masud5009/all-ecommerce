a85e0307c042bd4389a95ebb3777edac7e5eb72c
Masud Rana <ranaahmed269205@gmail.com>
2026-02-19 15:03:37 +0600

    df

--- resources/views/admin/transaction.blade.php
+++ resources/views/admin/transaction.blade.php
@@ -12,19 +12,13 @@
     </nav>
 
     <div class="col-lg-12">
-        <div class="card">
-            <div class="card-header">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="card-title">
-                            <h5>{{ __('All Transactions') }}</h5>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                    </div>
-                    <div class="col-lg-4">
-                        {{ $transactions->links() }}
-                    </div>
+        <div class="card transactions-card">
+            <div class="card-header transactions-card-header">
+                <div class="card-title mb-0">
+                    <h5 class="mb-0">{{ __('All Transactions') }}</h5>
+                </div>
+                <div class="transactions-pagination">
+                    {{ $transactions->links() }}
                 </div>
             </div>
             <div class="card-body">
@@ -32,17 +26,17 @@
                     @if (count($transactions) == 0)
                         <h5 class="text-center">{{ __('NO TRANSACTIONS FOUND') }} !</h5>
                     @else
-                        <div class="table-responsive">
-                            <table class="table table-striped">
+                        <div class="table-responsive transactions-table-wrapper">
+                            <table class="table table-striped align-middle transactions-table">
                                 <thead>
-                                    <th scope="col">{{ __('Transaction Id') }}</th>
-                                    <th scope="col">{{ __('Date') }}</th>
-                                    <th scope="col">{{ __('Transaction Type') }}</th>
-                                    <th scope="col">{{ __('Payment Method') }}</th>
-                                    <th scope="col">{{ __('Pre Balance') }}</th>
-                                    <th scope="col">{{ __('Add Balance') }}</th>
-                                    <th scope="col">{{ __('After Balance') }}</th>
-                                    <th scope="col">{{ __('Status') }}</th>
+                                    <th scope="col" class="text-nowrap">{{ __('Transaction Id') }}</th>
+                                    <th scope="col" class="text-nowrap">{{ __('Date') }}</th>
+                                    <th scope="col" class="text-nowrap">{{ __('Transaction Type') }}</th>
+                                    <th scope="col" class="text-nowrap">{{ __('Payment Method') }}</th>
+                                    <th scope="col" class="text-nowrap">{{ __('Pre Balance') }}</th>
+                                    <th scope="col" class="text-nowrap">{{ __('Add Balance') }}</th>
+                                    <th scope="col" class="text-nowrap">{{ __('After Balance') }}</th>
+                                    <th scope="col" class="text-nowrap">{{ __('Status') }}</th>
                                 </thead>
                                 <tbody>
                                     @foreach ($transactions as $transaction)
@@ -51,20 +45,20 @@
                                             $position = $transaction->currency_symbol_position;
                                         @endphp
                                         <tr>
-                                            <td>{{ '#' . $transaction->transaction_id }}</td>
-                                            <td>{{ $transaction->created_at }}</td>
+                                            <td class="text-nowrap">{{ '#' . $transaction->transaction_id }}</td>
+                                            <td class="text-nowrap">{{ $transaction->created_at }}</td>
                                             <td>{{ str_replace('_', ' ', ucwords($transaction->transaction_type)) }}</td>
-                                            <td>{{ $transaction->payment_method }}</td>
-                                            <td>{{ currency_symbol_order($transaction->pre_balance, $symbol, $position) }}
+                                            <td class="text-nowrap">{{ $transaction->payment_method }}</td>
+                                            <td class="text-nowrap">{{ currency_symbol_order($transaction->pre_balance, $symbol, $position) }}
                                             </td>
-                                            <td>
+                                            <td class="text-nowrap">
                                                 <span class="text-success">(+)</span>
                                                 {{ currency_symbol_order($transaction->actual_total, $symbol, $position) }}
                                             </td>
-                                            <td>
+                                            <td class="text-nowrap">
                                                 {{ currency_symbol_order($transaction->after_balance, $symbol, $position) }}
                                             </td>
-                                            <td>
+                                            <td class="text-nowrap">
                                                 @if ($transaction->transaction_type == 'product_purchase')
                                                     <span class="badge bg-success">{{ __('Paid') }}</span>
                                                 @else
